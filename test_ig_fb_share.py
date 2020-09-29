af0d993c3edad7f33b78c168056c26e2638d329d
radman-s <rads.spacek@gmail.com>
2020-09-29 14:44:53 +0200

    update

--- test_ig_fb_share.py
+++ test_ig_fb_share.py
@@ -22,11 +22,13 @@ ip = InstagramPage(driver=browser1)
 
 # test start
 ip.go()
+
 # login to ig
 ip.ig_user_input.input_text(user_ig)
 ip.ig_pw_input.input_text(pw_ig)
 ip.ig_login_button.click()
 ip.ig_notnow_button.click()
+
 # share img to fb
 ip.profile_link.click()
 ip.image.click()
@@ -39,10 +41,11 @@ browser1.switch_to.window(browser1.window_handles[1])
 ip.email_fb_input.input_text(user_gmail)
 ip.pw_fb_input.input_text(pw_gmail)
 ip.login_fb_button.click()
-# name img to be shared
+
+# name and post img to be shared
 ip.comment_img.input_text(ig_img_name)
-#
 ip.post_fb_button.click()
+
 # switch back to ig tab
 browser1.switch_to.window(browser1.window_handles[0])
 ip.close_share.click()
@@ -52,14 +55,16 @@ ip.close_image.click()
 browser2 = Drivers('--start-maximized').chrome()
 fp = FacebookPage(driver=browser2)
 fp.go()
+
 # login to fb
 fp.fb_user_input.input_text(user_gmail)
 fp.fb_pw_input.input_text(pw_gmail)
 fp.fb_login_button.click()
-# go to shared image
+
+# go to shared image and get img name
 fp.profile_button.click()
-# get the image name
 fb_img_name = fp.img_name.text
+
 # validate image name
 assert fb_img_name == ig_img_name
 print(f'fb image name "{fb_img_name}" correspond to ig image name "{ig_img_name}"')
