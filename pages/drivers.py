92f33ce73cc6dae500058f44a9e3c5074f0bed4f
radman-s <rads.spacek@gmail.com>
2020-09-29 14:53:35 +0200

    update

--- pages/drivers.py
+++ pages/drivers.py
@@ -5,11 +5,8 @@ class Drivers:
     def __init__(self, argument):
         self.argument = argument
 
-
     def chrome(self):
         options = webdriver.ChromeOptions()
-        prefs = {"profile.default_content_setting_values.notifications": 2}
-        options.add_experimental_option("prefs", prefs)
         options.add_argument(self.argument)
         driver = webdriver.Chrome(options=options)
         return driver
